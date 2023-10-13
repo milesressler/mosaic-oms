6d0f78bd5cb9e451778e6cd23a4450132b23fcb0
Miles Ressler <milesressler@gmail.com>
2023-10-13 12:11:32 -0500

    Initial spring boot project setup; sample controller;

---
+++ src/main/java/com/mosaicchurchaustin/oms/controllers/OrderController.java
@@ -0,0 +1,18 @@
+package com.mosaicchurchaustin.oms.controllers;
+
+import com.mosaicchurchaustin.oms.data.responses.OrderResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @ResponseBody
+    @GetMapping(path = "/order/{id}", produces = "application/json")
+    public OrderResponse submitOrder(@PathVariable("id") final String orderId) {
+        return OrderResponse.builder().id("d").build();
+    }
+}
+
---
+++ src/main/java/com/mosaicchurchaustin/oms/data/responses/OrderResponse.java
@@ -0,0 +1,10 @@
+package com.mosaicchurchaustin.oms.data.responses;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class OrderResponse {
+    private String id;
+}
