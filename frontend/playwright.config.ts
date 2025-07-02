ffe54fd105a843f0dbc39589daa4e370f36b9716
Miles Ressler <milesressler@gmail.com>
2025-07-02 14:39:02 -0500

    manual dispatch

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from '@playwright/test';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+function storageStateOrEmpty(filePath: string) {
+    return fs.existsSync(filePath)
+        ? filePath
+        : { cookies: [], origins: [] };
+}
+
 export default defineConfig({
     testDir: 'e2e',
     timeout: 15000,
@@ -10,21 +16,21 @@ export default defineConfig({
             name: 'local',
             use: {
                 baseURL: process.env.LOCAL_URL,
-                storageState: 'e2e/data/local-storage.json',
+                storageState: storageStateOrEmpty('e2e/data/local-storage.json'),
             },
         },
         {
             name: 'qa',
             use: {
                 baseURL: process.env.QA_URL,
-                storageState: 'e2e/data/qa-storage.json',
+                storageState: storageStateOrEmpty('e2e/data/qa-storage.json'),
             },
         },
         {
             name: 'prod',
             use: {
                 baseURL: process.env.PROD_URL,
-                storageState: 'e2e/data/prod-storage.json',
+                storageState: storageStateOrEmpty('e2e/data/prod-storage.json'),
             },
         },
     ],
